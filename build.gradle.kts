cd924857a3bb217f61cf8fc6c2d4e9fd49a1cc1a
SiteStatic <38054885+Morganczech@users.noreply.github.com>
2025-02-13 22:07:44 +0100

    První commit na macbooku

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.8.0")
+        classpath("com.android.tools.build:gradle:8.8.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.20-1.0.14")
